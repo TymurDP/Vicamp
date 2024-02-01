5bc392ca47397543059710488cc5f0fff5ed0fe1
TymurDP <149867142+TymurDP@users.noreply.github.com>
2024-02-01 14:27:03 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,13 @@
-let resSlide = document.getElementsByClassName("residential__item");
-let resButtonLeft = document.getElementById("rbl");
-let resButtonRight = document.getElementById("rbr");
+let resSlide = document.querySelectorAll(".residential__item");
+let resButtonLeft = document.querySelector("#rbl");
+let resButtonRight = document.querySelector("#rbr");
 
-let comSlide = document.getElementsByClassName("commercial__item");
-let comButtonLeft = document.getElementById("cbl");
-let comButtonRight = document.getElementById("cbr");
+let comSlide = document.querySelectorAll(".commercial__item");
+let comButtonLeft = document.querySelector("#cbl");
+let comButtonRight = document.querySelector("#cbr");
 
-let disable = 'opacity: 0.2;cursor: not-allowed;';
-let visible = 'opacity: 1;cursor: pointer';
-
+let disable = "opacity: 0.2;cursor: not-allowed;";
+let visible = "opacity: 1;cursor: pointer";
 
 let slideClick = 0;
 let k = 0;
@@ -16,66 +15,88 @@ let k = 0;
 let slideClickCom = 0;
 let kCom = 0;
 
-let transTranslate = 130;
+let transTranslate = 135;
 resButtonLeft.style = disable;
 comButtonLeft.style = disable;
 
-let dropButton = document.getElementsByClassName("dropButton");
-let dropText = document.getElementsByClassName("dropText");
+let dropButton = document.querySelectorAll(".dropButton");
+let dropText = document.querySelectorAll(".dropText");
 
+resButtonLeft.onclick = () => {
+  if (slideClick < 0) {
+    slideClick++;
+    k = slideClick * transTranslate;
+    for (let i = 0; i < resSlide.length; i++) {
+      resSlide[i].style = "transform: translate(" + k + "px);";
+    }
+    resButtonRight.style = visible;
+    slideClick === 0
+      ? (resButtonLeft.style = disable)
+      : (resButtonLeft.style = visible);
+  }
+};
 
-document.getElementById("rbl").onclick = function () {
-    if (slideClick < 0) {
-        slideClick++;
-        k = slideClick * transTranslate;
-        for (let i = 0; i < resSlide.length; i++) {resSlide[i].style = 'transform: translate(' + k + 'px);';}
-        resButtonRight.style = visible;
-        if (slideClick === 0) { resButtonLeft.style = disable;}
-    } else { resButtonLeft.style = disable;}
-} 
-    
-document.getElementById("rbr").onclick = function () {
-    if (slideClick > -3) {
-        slideClick--;
-        k = slideClick * transTranslate;
-        for (let i = 0; i < resSlide.length; i++) {resSlide[i].style = 'transform: translate(' + k + 'px);';}
-        resButtonLeft.style = visible;
-        if (slideClick === -3) {resButtonRight.style = disable;}
-    } else {resButtonRight.style = disable;}
-}
+resButtonRight.onclick = () => {
+  if (slideClick > -3) {
+    slideClick--;
+    k = slideClick * transTranslate;
+    for (let i = 0; i < resSlide.length; i++) {
+      resSlide[i].style = "transform: translate(" + k + "px);";
+    }
+    resButtonLeft.style = visible;
+    slideClick === -3
+      ? (resButtonRight.style = disable)
+      : (resButtonRight.style = visible);
+  }
+};
 
-document.getElementById("cbl").onclick = function () {
-    if (slideClickCom < 0) {
-        slideClickCom++;
-        kCom = slideClickCom * transTranslate;
-        console.log(kCom);
-        for (let i = 0; i < comSlide.length; i++) {comSlide[i].style = 'transform: translate(' + kCom + 'px);';}
-        comButtonRight.style = visible;
-        if (slideClickCom === 0) { comButtonLeft.style = disable;}
-    } else { comButtonLeft.style = disable;}
-} 
-    
-document.getElementById("cbr").onclick = function () {
-    if (slideClickCom > -2) {
-        slideClickCom--;
-        kCom = slideClickCom * transTranslate;
-        for (let i = 0; i < comSlide.length; i++) {comSlide[i].style = 'transform: translate(' + kCom + 'px);';}
-        comButtonLeft.style = visible;
-        if (slideClickCom === -2) {comButtonRight.style = disable;}
-    } else {comButtonRight.style = disable;}
-}
+comButtonLeft.onclick = () => {
+  if (slideClickCom < 0) {
+    slideClickCom++;
+    kCom = slideClickCom * transTranslate;
+    for (let i = 0; i < comSlide.length; i++) {
+      comSlide[i].style = "transform: translate(" + kCom + "px);";
+    }
+    comButtonRight.style = visible;
+    slideClickCom === 0
+      ? (comButtonLeft.style = disable)
+      : (comButtonLeft.style = visible);
+  }
+};
+
+comButtonRight.onclick = () => {
+  if (slideClickCom > -2) {
+    slideClickCom--;
+    kCom = slideClickCom * transTranslate;
+    for (let i = 0; i < comSlide.length; i++) {
+      comSlide[i].style = "transform: translate(" + kCom + "px);";
+    }
+    comButtonLeft.style = visible;
+    slideClickCom === -2
+      ? (comButtonRight.style = disable)
+      : (comButtonRight.style = visible);
+  }
+};
 
 for (let i = 0; i < dropButton.length; i++) {
-    dropText[i].style.display = 'none';
-    dropButton[i].onclick = function () {
-        if (dropText[i].style.display == 'none') {
-            dropText[i].style.display = 'block';
-            dropButton[i].style = 'transform: rotate(180deg);transition: 0.3s;';
-        } else {
-            dropText[i].style.display = 'none';
-            dropButton[i].style = 'transform: rotate(0deg);transition: 0.3s;';
+  dropText[i].style.display = "none";
+  dropButton[i].onclick = () => {
+    if (dropText[i].style.display == "none") {
+      dropText[i].style.display = "block";
+      dropButton[i].style = "transform: rotate(180deg);transition: 0.3s;";
+    } else {
+      dropText[i].style.display = "none";
+      dropButton[i].style = "transform: rotate(0deg);transition: 0.3s;";
     }
-    
-    }
-
+  };
 }
+
+// function startup() {
+//     el = document.getElementsByClassName("residential__item");
+//     el.addEventListener('touchstart', () => { console.log('start') }); // el.ontouchstart = () => { console.log('start') };
+//     el.addEventListener('touchend', () => { console.log('end') }); // el.ontouchstart = () => { console.log('start') };
+//     el.addEventListener('touchmove', () => { console.log('move') }); // el.ontouchstart = () => { console.log('start') };
+//     el.addEventListener('touchcancel', () => { console.log('cancel') }); // el.ontouchstart = () => { console.log('start') };
+// }
+
+// document.addEventListener("DOMContentLoaded", startup);
